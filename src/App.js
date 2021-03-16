afa2c36cfe004a17034682e3db5f7bc1ffd087df
ben <noahbenaas@gmail.com>
2021-03-16 16:01:26 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CTA from "./CTA";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <CTA>
+          <h1>This is an important CTA</h1>
+          <button>Click me now or you'll miss out!</button>
+        </CTA>
+        <br />
+        <CTA>
+          <form>
+            <input type="email" placeholder="Enter email address here" />
+            <br />
+            <button>Submit</button>
+          </form>
+        </CTA>
       </header>
     </div>
   );
